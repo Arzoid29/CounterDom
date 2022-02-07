07722fb2a7c492bb8b0f5a3dc39551ca36d5b164
José David Sánchez <49158320+Arzoid29@users.noreply.github.com>
2022-02-07 15:29:18 -0400

    Doing a counter to pratice dom manipulation

---
+++ index.js
@@ -0,0 +1,29 @@
+//set inicial count to 0
+let count = 0;
+
+// select values and bottons from html
+const value = document.querySelector('#value');
+const btns= document.querySelectorAll('.btn');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click',function(e){
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+        count--;
+    }else if(styles.contains('increase')){
+        count++;
+    }else{
+        count = 0;
+    }
+    if(count > 0){
+        value.style.color ='Green'
+    }
+    if(count < 0){
+        value.style.color ='Red'
+    }
+    if(count === 0){
+        value.style.color ='Black'
+    }
+    value.textContent= count;
+    })
+});
